35a1a98c5d070fcac6c5cfef198495025fcf83d4
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-11 11:32:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,13 @@
 class Triangle
   # write code here
+  def initialize(length, breadth, height)
+    @length = length
+    @breadth = breadth
+    @height = height
+  end 
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
 
